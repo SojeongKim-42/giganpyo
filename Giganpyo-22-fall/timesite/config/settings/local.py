02f74315de7548043b3caba05904e0377a412934
jclee0109 <jclee0109@gist.ac.kr>
2022-07-29 16:46:53 +0900

    setting changed

--- Giganpyo-22-fall/timesite/config/settings/local.py
+++ Giganpyo-22-fall/timesite/config/settings/local.py
@@ -1,10 +1,10 @@
 from .base import *
 import my_settings
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 DEBUG = True
 
 SITE_ID = my_settings.SITE_ID 
 
-
+# ACCOUNT_DEFAULT_HTTP_PROTOCOL = 'http'
--- Giganpyo-22-fall/timesite/config/settings/prod.py
+++ Giganpyo-22-fall/timesite/config/settings/prod.py
@@ -5,6 +5,6 @@ ALLOWED_HOSTS = ['15.165.225.220', 'giganpyo.co.kr', 'localhost']
 
 SITE_ID = my_settings.SITE_ID
 
-DEBUG = False
+DEBUG = True
 
 ACCOUNT_DEFAULT_HTTP_PROTOCOL='https'
