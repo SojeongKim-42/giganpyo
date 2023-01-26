448f3801f1f54337c0fdfce40ddc5ae941ec1f1c
sojeong <k5k5s5s5j5j5@naver.com>
2023-01-26 18:20:52 +0000

    feat(accountapp, giganpyo/settings): 배포 환경 세팅

--- accountapp/text.py
+++ accountapp/text.py
@@ -1,4 +1,4 @@
 def message(domain, uidb64, token):
-    email_body = f"http://http://ec2-54-180-104-168.ap-northeast-2.compute.amazonaws.com/api/user/activate/{uidb64}/{token}"
+    email_body = f"http://ec2-54-180-104-168.ap-northeast-2.compute.amazonaws.com/api/user/activate/{uidb64}/{token}"
     
     return email_body
