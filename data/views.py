5c57862584c4729a0cf290299cbc4341b27c095f
kim sojeong <78066907+SojeongKim-42@users.noreply.github.com>
2023-01-28 04:14:19 +0900

    Feat/data save (#27)
    * feat(data): 학점 추가
    
    * feat(data): 엑셀 불러오기 최적화, json update
    
    - 하드코딩되어있던 숫자를 column명으로 검색해서 가져오기

--- data/views.py
+++ data/views.py
@@ -1,35 +1,47 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from subjectapp.models import *
+from parse import *
 
 import pandas as pd
 import re
 import numpy
 
 
-def data_save(request):
+def data_save():
     # 엑셀파일 받기
     Location = './'
     # 이거 바꿔줄 필요 있음
-    File = '22_fall.xls'
+    File = '23_spring.xls'
 
-    year = 2022
-    session = "fall"
+    year = 2023
+    session = "spring"
 
-    data_pd = pd.read_excel('{}/{}'.format(Location, File),
-                            header=None, index_col=None, names=None)
+    data_pd = pd.read_excel('{}/{}'.format(Location, File),header=None, index_col=None, names=None)
 
-    # 시간정보 읽어오기
+    t_data=data_pd.T
+    columns = t_data[0]
+    DEPARTMENT = t_data[columns == '개설부서'].index[0]
+    CODE = t_data[columns == '교과목-분반'].index[0]
+    NAME = t_data[columns == '교과목명'].index[0]
+    REQUIRED = t_data[columns == '이수\n구분'].index[0]
+    PROFESSOR = t_data[columns == '담당교수'].index[0]
+    LEC_EXP_CRE = t_data[columns == '강/실/학'].index[0]
+    TIME = t_data[columns == '시간표'].index[0]
+    LOCATION = t_data[columns == '강의실'].index[0]
+    MAX = t_data[columns == '수강\n정원'].index[0]
+    OFFLINE = t_data[columns == '강의\n방법'].index[0]
+    MAJOR = t_data[columns == '교과\n연구'].index[0]
+
+    # 시간정보, 요일정보 읽어오기
     times = []
-    for i in range(1, len(data_pd)):
-        times.append(re.findall("\d+", str(data_pd[13][i])))
-    for i in range(len(times)):
-        times[i] = numpy.array(times[i]).reshape(len(times[i]) // 4, 2, 2)
-
-    # 요일정보 읽어오기
     days = []
     for i in range(1, len(data_pd)):
-        days.append(re.compile("[가-힣]+").findall(str(data_pd[13][i])))
+        times.append(re.findall("\d+", str(data_pd[TIME][i])))
+        days.append(re.compile("[가-힣]+").findall(str(data_pd[TIME][i])))
+
+    for i in range(len(times)):
+        times[i] = numpy.array(times[i]).reshape(len(times[i]) // 4, 2, 2)
 
     # Time 중복 제거
     all_times_days = []
@@ -54,56 +66,34 @@ def data_save(request):
     # 교수님정보 읽어오기
     profs = []
     for i in range(1, len(data_pd)):
-        data_pd[9][i] = str(data_pd[9][i]).replace(" ", "")
-        profs.append(re.compile("[가-힣]+").findall(data_pd[9][i]))
+        data_pd[PROFESSOR][i] = str(data_pd[PROFESSOR][i]).replace(" ", "")
+        profs.append(re.compile("[가-힣]+").findall(data_pd[PROFESSOR][i]))
 
     # Prof 중복 제거
     unique_profs = list(set(sum(profs, [])))
 
-    # 과목명 읽어오기
+    # 과목명, 과목코드, 학점정보, 개설부서, 필수여부, 전공 여부, 대면 비대면, 강의실, 수강정원
     names = []
-    for i in range(1, len(data_pd)):
-        names.append(data_pd[5][i])
-
-    # 과목코드 읽어오기
     codes = []
-    for i in range(1, len(data_pd)):
-        codes.append(data_pd[4][i])
-
-    # 학점정보 읽어오기
     credits = []
-    for i in range(1, len(data_pd)):
-        credits.append(int(data_pd[30][i]))
-
-    # 개설부서 읽어오기
     departments = []
-    for i in range(1, len(data_pd)):
-        departments.append(data_pd[3][i])
-
-    # 필수여부 읽어오기
     is_requireds = []
-    for i in range(1, len(data_pd)):
-        is_requireds.append(data_pd[6][i])
-
-    # 교양, 전공 읽어오기
     is_majors = []
-    for i in range(1, len(data_pd)):
-        is_majors.append(data_pd[8][i])
-
-    # 대면, 비대면 읽어오기
     is_offlines = []
-    for i in range(1, len(data_pd)):
-        is_offlines.append(data_pd[26][i])
+    locations = []
+    max_persons = []
 
-    # 강의실 위치 읽어오기
-    locations=[]
     for i in range(1, len(data_pd)):
-        locations.append(data_pd[14][i])
-
-    # 강의실 위치 읽어오기
-    max_persons=[]
-    for i in range(1, len(data_pd)):
-        max_persons.append(data_pd[15][i])
+        names.append(data_pd[NAME][i])
+        codes.append(data_pd[CODE][i])
+        parsed = parse("{lec}/{exp}/{cre}", str(data_pd[LEC_EXP_CRE][i]))
+        credits.append(int(parsed["cre"]))   
+        departments.append(data_pd[DEPARTMENT][i])    
+        is_requireds.append(data_pd[REQUIRED][i])    
+        is_majors.append(data_pd[MAJOR][i])    
+        is_offlines.append(data_pd[OFFLINE][i])
+        locations.append(data_pd[LOCATION][i])
+        max_persons.append(data_pd[MAX][i])
 
     # Professor 채우기
     for i in range(len(unique_profs)):
