8083182a7e867c2bc5887826371ba8b228fd667f
jclee0109 <jclee0109@gist.ac.kr>
2023-01-28 03:54:58 +0900

    fix(giganpyo/celery.py): celery setting

--- giganpyo/celery.py
+++ giganpyo/celery.py
@@ -3,11 +3,11 @@ import os
 from celery import Celery
 from giganpyo.settings import base as settings
 
-if settings.DEBUG:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'giganpyo.settings.development')
-# Set the default Django settings module for the 'celery' program.
-else:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'giganpyo.settings.production')
+# if settings.DEBUG:
+#     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'giganpyo.settings.development')
+# # Set the default Django settings module for the 'celery' program.
+# else:
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'giganpyo.settings.production')
 
 app = Celery('giganpyo')
 
