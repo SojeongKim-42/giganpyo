75a6b5a4e24131b7dabadfcfde1b58cbc92530ac
Sojeong <k5k5s5s5j5j5@naver.com>
2023-01-28 15:13:32 +0900

    hotfix(settings): cors middleware 위치 변경

--- giganpyo/settings/base.py
+++ giganpyo/settings/base.py
@@ -163,14 +163,14 @@ DJANGO_CELERY_RESULTS_TASK_ID_MAX_LENGTH = 191
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'giganpyo.urls'
