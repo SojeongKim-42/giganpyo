94f3ef2617e7be41688fcaf739b5547765fe0b81
kim sojeong <78066907+SojeongKim-42@users.noreply.github.com>
2023-02-04 20:31:44 +0900

    feat(*/admin): admin페이지 검색 기능 추가 (#62)

--- subjectapp/admin.py
+++ subjectapp/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from subjectapp.models import Subject, Cart
 
-admin.site.register(Subject)
-admin.site.register(Cart)
+class SubjectAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'code']
+    list_display=['subject_id','name','code','select_person']
+
+class CartAdmin(admin.ModelAdmin):
+    search_fields = ['table_id']
+    list_display=['id','subject_id','table_id']
+
+admin.site.register(Subject, SubjectAdmin)
+admin.site.register(Cart, CartAdmin)
 
 # Register your models here.
--- tableapp/admin.py
+++ tableapp/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from tableapp.models import Table
 
+
+class TableAdmin(admin.ModelAdmin):
+    search_fields = ['user_id']
+    list_display=['table_id','user_id','main','name']
+
 admin.site.register(Table)
 # Register your models here.
