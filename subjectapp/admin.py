e1bec80d841fdba5052f50d48693160925f0ca18
Sojeong <k5k5s5s5j5j5@naver.com>
2023-02-08 01:46:07 +0900

    hotfix: admin page update&professor model update

--- subjectapp/admin.py
+++ subjectapp/admin.py
@@ -6,10 +6,20 @@ class SubjectAdmin(admin.ModelAdmin):
     list_display=['subject_id','name','code','select_person']
 
 class CartAdmin(admin.ModelAdmin):
-    search_fields = ['table_id']
+    search_fields = ['subject_id']
     list_display=['id','subject_id','table_id']
+
+class TimeAdmin(admin.ModelAdmin):
+    search_fields = ['id','day']
+    list_display=['id','day','start_time','fin_time']
+
+class ProfessorAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display=['id','name']
 
 admin.site.register(Subject, SubjectAdmin)
 admin.site.register(Cart, CartAdmin)
+admin.site.register(Time, TimeAdmin)
+admin.site.register(Professor, ProfessorAdmin)
 
 # Register your models here.
--- subjectapp/models.py
+++ subjectapp/models.py
@@ -8,6 +8,9 @@ class Professor(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class Time(models.Model):
     id = models.AutoField(primary_key=True)
     day = models.CharField(max_length=1)
